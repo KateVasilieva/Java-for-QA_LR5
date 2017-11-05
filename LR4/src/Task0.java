9035eeb8d37cc08f7bc513178a24d7e804ec387b
KateVasilieva <vasilieva.katerina26@gmail.com>
2017-11-05 12:36:54 +0200

    commit LR 4 Task1

--- LR4/src/Task0.java
+++ LR4/src/Task0.java
@@ -1,5 +1,7 @@
 public class Task0 {
     public static void main(String[] args) {
+        //написать условие, которое сравнит две переменные int low, int high и выведет в консоль текст Уlow is smaller then highФ
+        // если число в переменной low меньше числа в переменной high и аналогично, если все будет наоборот.
         int low = 6;
         int high = 5;
         if (low < high) {
@@ -8,7 +10,7 @@ public class Task0 {
             System.out.println("high is smaller then low");
         }
 
-
+// создать массив test на 10 элементов и с помощью цикла распечатать его.
         int[] test = new int[10];
         for (int i = 0; i < 10; i++) {
             test[i] = i + 10;
--- LR4/src/Task1.java
+++ LR4/src/Task1.java
@@ -1,13 +1,18 @@
-public class Task1 {
-
-    public int solution(int number) {
+public class Task1  {
+    public static void main(String[] args) {
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+        // Note: If the number is a multiple of both 3 and 5, only count it once.
+        //(1 % 3) == 0 - проверка будет ли остаток при делении 1 на 3. Ответ - будет и это означает, что число не делиться на 3 без остатка.
 
         int sum = 0;
-       for ( (number % 3) == 0 or (number % 5) == 0)
-           ВАШ КОД
-          }
-       }
+        int i;
+        for (i = 1; i < 11; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                    sum = sum + i;
+            }
 
-        return sum;
+            System.out.println(sum);
+        }
+
     }
-}
